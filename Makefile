80185a4990564fae9f642c2a7d29c0eddd5f16d3
santiagoVL03 <santiagovl0308@gmail.com>
2022-11-28 21:30:37 -0500

    segment_tree_base

---
+++ Makefile
@@ -0,0 +1,2 @@
+run-code:
+	g++ segment_tree.cpp && .\a.exe && dot -Tpng tree.dot > tree.png
---
+++ segment_tree.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+template <class T>
+struct segment_tree
+{
+private:
+    T *root;                  /*Pointer to the root*/
+    int srArraySize;
+    int originalArraySize;
+
+public:
+    segment_tree(vector<T>)
+    {
+
+    }
+    ~segment_tree()
+    {
+        
+    }
+
+};
+
+
+
+int main()
+{
+    return 0;
+}
